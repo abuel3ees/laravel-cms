c610e2fef9c457c544c718bd4e75995697c27eee
abuel3ees <abdurahmanluay@gmail.com>
2025-07-29 15:47:53 +0300

    Global Soft Delete

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -10,7 +10,7 @@
 <div class="container mt-5" style="max-width: 700px; color: #ff4333; font-family: 'space mono', monospace;">
     <form method="POST" action="{{ route('articles.store') }}" enctype="multipart/form-data" class="p-4 rounded shadow" style="background-color: #1a1a1a; border: 1px solid #2a2a2a; color: #ff4333;">
         @csrf
-        <div class="mb-4">
+        <div class="mb-4" style="color: #ff4333">
           <x-createform />
 </div>
 
