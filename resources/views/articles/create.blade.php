2850ae5a1f763ff1ad96cacea33532bcfd34f293
abuel3ees <abdurahmanluay@gmail.com>
2025-07-29 15:24:39 +0300

    UI Changes

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -1,14 +1,14 @@
 @extends('dashboard')
 
-@section('title', 'Create Article')
+@section('title', 'CREATE_ARTICLE')
 
 @section('content')
 
 <!-- External CSS -->
 <link href="{{ asset('css/create-article.css') }}" rel="stylesheet">
 
-<div class="container mt-5" style="max-width: 700px; font-family: 'Roboto Condensed', sans-serif;">
-    <form method="POST" action="{{ route('articles.store') }}" enctype="multipart/form-data" class="p-4 rounded shadow" style="background-color: #1a1a1a; border: 1px solid #2a2a2a;">
+<div class="container mt-5" style="max-width: 700px; color: #ff4333; font-family: 'space mono', monospace;">
+    <form method="POST" action="{{ route('articles.store') }}" enctype="multipart/form-data" class="p-4 rounded shadow" style="background-color: #1a1a1a; border: 1px solid #2a2a2a; color: #ff4333;">
         @csrf
         <div class="mb-4">
           <x-createform />
